483b3daf302f2a89f1be842f023888f80f5d21a9
noilaibodullayeva <noilaibodullayeva07@gmail.com>
2024-02-12 10:02:23 -0800

    ...

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import './App.css';
 import Header from './header/Header';
 import Section3 from './section 2/section3';
 import Home from './home/Home';
-import slides from "../src/home/mock.json"
 
 function App() {
   return (
     <Box>
       <Header />
-      <Home slides={slides}/>
+      <Home />
       <Section3 />
     </Box>
   );
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,28 +1,80 @@
+import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
-import './swiper.css';
-import { Navigation, Pagination, Scrollbar, Ally } from 'swiper'
+import 'swiper/css';
+import 'swiper/css/navigation';
 
+import './styles.css';
 
-export default function Home({ slides }) {
+// import required modules
+import { Navigation } from 'swiper/modules';
+import { Box, Button, Typography } from '@mui/material';
+import IMG6 from "../img/master-slides-01.jpg.webp"
+import IMG7 from "../img/master-slides-02.jpg.webp"
+import IMG8 from "../img/slide1-01.jpg.webp"
+
+export default function Home() {
   return (
-    <Swiper
-      modules={[Navigation, Pagination, Scrollbar, Ally]}
-      spaceBetween={50}
-      slidesPerView={3}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      {slides.map((slide) => (
-        <SwiperSlide key={slide.image}>
-          <img src={slide.image} alt={slide.title} className='imgg' />
+    <>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide>
+          <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundImage: `url(${IMG6})`,
+            width: '100%',
+            height: '670px',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+          }}>
+            <Typography sx={styles.welcome}>Welcome to</Typography>
+            <Typography sx={styles.pato}>PATO PLACE</Typography>
+            <Button sx={styles.buttonSlide}>LOOK MENU</Button>
+          </Box>
         </SwiperSlide>
-      ))}
-    </Swiper>
+        <SwiperSlide>
+          <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundImage: `url(${IMG7})`,
+            width: '100%',
+            height: '670px',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+          }}>
+            <Typography sx={styles.welcome}>Welcome to</Typography>
+            <Typography sx={styles.pato}>PATO PLACE</Typography>
+            <Button sx={styles.buttonSlide}>LOOK MENU</Button>
+          </Box>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundImage: `url(${IMG8})`,
+            width: '100%',
+            height: '670px',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+          }}>
+            <Typography sx={styles.welcome}>Welcome to</Typography>
+            <Typography sx={styles.pato}>PATO PLACE</Typography>
+            <Button sx={styles.buttonSlide}>LOOK MENU</Button>
+          </Box>
+        </SwiperSlide>
+      </Swiper>
+    </>
   );
-};
+}
+
+const styles = {
+
+}
